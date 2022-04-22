b8c505afdda7d781bad5b40df30069c5ab2be629
blackcodefan <myongilk@outlook.com>
2022-04-22 12:12:47 +0800

    fix/04-22-22/logout issue

--- src/pages/api/user/logout.ts
+++ src/pages/api/user/logout.ts
@@ -5,7 +5,9 @@ export default withSessionRoute(handler);
 function handler(req: any, res: any) {
     if (req.session.user){
         req.session.destroy();
-        return res.status(200).json({success: true});
+        req.session.save().then(() =>{
+            return res.status(200).json({success: true});
+        });
     }
     else
         res.status(401).json({success: false, message: 'Not Authorized'});
